c8f8822f262bfeaea1ba879760328ff66877d730
NSergeiV <ttniseva@yandex.ru>
2019-10-05 17:17:38 +0300

    Выполниел учебный проект Лекции №4. Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+// userDialog.classList.remove('hidden');
 
 var nameWizard = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnameWizard = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -51,3 +51,87 @@ var creatingCopies = function () {
 };
 
 creatingCopies();
+
+// Взаимодействие пользователя с сайтом, открытие окна попап настроек персонжа
+
+var iconAvatar = document.querySelector('.setup-open');
+var iconCloseUserDialog = userDialog.querySelector('.setup-close');
+var iconAvatarFocus = iconAvatar.querySelector('.setup-open-icon');
+var inputName = userDialog.querySelector('.setup-user-name');
+
+var KEY_ENTER = 13;
+var KEY_ESC = 27;
+
+var onUserDialogClose = function (evt) {
+  if (evt.keyCode === KEY_ESC) {
+    closePopap();
+  }
+};
+
+var onUserDialogFocusOpen = function (evt) {
+  if (iconAvatarFocus === document.activeElement) {
+    if (evt.keyCode === KEY_ENTER) {
+      openPopap();
+    }
+  }
+};
+
+var onUserDialogFocusClose = function (evt) {
+  if (iconCloseUserDialog === document.activeElement) {
+    if (evt.keyCode === KEY_ENTER) {
+      closePopap();
+    }
+  }
+};
+
+var onFormCancelSubmit = function (evt) {
+  if (inputName === document.activeElement) {
+    if (evt.keyCode === KEY_ENTER) {
+      evt.preventDefault();
+    }
+  }
+};
+
+var openPopap = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onUserDialogClose);
+  document.addEventListener('keydown', onUserDialogFocusClose);
+  document.removeEventListener('keydown', onUserDialogFocusOpen);
+  document.addEventListener('keydown', onFormCancelSubmit, false);
+};
+
+var closePopap = function () {
+  if (inputName !== document.activeElement) {
+    userDialog.classList.add('hidden');
+    document.removeEventListener('keydown', onUserDialogClose);
+    document.removeEventListener('keydown', onUserDialogFocusClose);
+    document.removeEventListener('keydown', onFormCancelSubmit, false);
+    document.addEventListener('keydown', onUserDialogFocusOpen);
+  }
+};
+
+iconAvatar.addEventListener('click', openPopap);
+document.addEventListener('keydown', onUserDialogFocusOpen);
+
+iconCloseUserDialog.addEventListener('click', closePopap);
+
+// Настройка вида персонажа в попап
+
+var setupWizardMain = document.querySelector('.setup-wizard');
+var colorCoatWizardMain = setupWizardMain.querySelector('.wizard-coat');
+var colorEyesWizardMain = setupWizardMain.querySelector('.wizard-eyes');
+var colorFireballWrap = document.querySelector('.setup-fireball-wrap');
+
+var colorsForFireball = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+colorCoatWizardMain.addEventListener('click', function () {
+  document.querySelector('input[name="coat-color"][value]').value = colorCoatWizardMain.style.fill = randomCase(coatColorWizard);
+});
+
+colorEyesWizardMain.addEventListener('click', function () {
+  document.querySelector('input[name="eyes-color"][value]').value = colorEyesWizardMain.style.fill = randomCase(eyesColorWizard);
+});
+
+colorFireballWrap.addEventListener('click', function () {
+  document.querySelector('input[name="fireball-color"][value]').value = colorFireballWrap.style.background = randomCase(colorsForFireball);
+});
